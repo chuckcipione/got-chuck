2184b128b2c9fa85a94911ae8bd5a7e7c0f9b572
Chuck <chuckcipione@yahoo.com>
2017-09-12 14:31:14 -0500

    got-project

---
+++ js/kingslanding/gotCtrl.js
@@ -0,0 +1,3 @@
+angular.module('app').controller('gotCtrl', function($scope){
+    $scope.test = 'Winter is Coming';
+})
---
+++ js/kingslanding/gotSrv.js
@@ -0,0 +1,3 @@
+angular.module('app').service('gotSrv', function($http){
+
+})
