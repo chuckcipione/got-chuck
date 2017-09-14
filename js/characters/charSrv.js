b96a07d8b08ec2beb6be616c360e98ba57ecd9bd
Chuck <chuckcipione@yahoo.com>
2017-09-14 11:59:55 -0500

    got

--- js/characters/charSrv.js
+++ js/characters/charSrv.js
@@ -1,7 +1,20 @@
 angular.module('app').service('charSrv', function($http, $q){
-        
-        this.getAllChar = function(){
-            return $http.get("https://www.anapioficeandfire.com/api/characters?page=4&pageSize=70");
+        var pageCount = 1;    
+        var charContainer = [];
+        var pageSize = '&pageSize=50';        
+        var deferred = $q.defer();        
+        this.getAllChar = () => {
+             $http.get("https://www.anapioficeandfire.com/api/characters?page=" + pageCount + pageSize).then(response => {
+                if (response.data.length === 50) {
+                    pageCount++;
+                    charContainer.push(...response.data);
+                    this.getAllChar();
+                } else {
+                    charContainer.push(...response.data);
+                    deferred.resolve(charContainer);
+                }
+            });
+            return deferred.promise;
         }
         // this.getChar = function(url){
         //     return $http.get('https://www.anapioficeandfire.com/api/characters?page=3&pageSize=50rl');
