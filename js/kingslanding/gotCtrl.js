47f3392a3e74ab9822350b946b609d481edcce19
Chuck <chuckcipione@yahoo.com>
2017-09-18 09:24:31 -0500

    done

--- js/kingslanding/gotCtrl.js
+++ js/kingslanding/gotCtrl.js
@@ -1,3 +1,3 @@
 angular.module('app').controller('gotCtrl', function($scope){
-    $scope.splash = '/images/the-three-eyed-raven-game-of-thrones-31615-1920x1080.jpg';
+    $scope.splash = 'images/the-three-eyed-raven-game-of-thrones-31615-1920x1080.jpg';
 })
