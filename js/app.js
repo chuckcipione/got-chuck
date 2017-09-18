68bf03f524cbbb14b5a1afafb46f094a162f9051
Chuck <chuckcipione@yahoo.com>
2017-09-18 09:20:10 -0500

    done

--- js/app.js
+++ js/app.js
@@ -4,12 +4,12 @@ angular.module('app', ['ui.router'])
     $stateProvider
         .state('home', {
             url: '/',
-            templateUrl: '/js/kingslanding/kingslanding.html',
+            templateUrl: 'js/kingslanding/kingslanding.html',
             controller: 'gotCtrl'
         })
         .state('characters', {
             url: '/characters',
-            templateUrl: '/js/characters/characters.html',
+            templateUrl: 'js/characters/characters.html',
             controller: 'charCtrl',
             // resolve: {
             //     characters: function(charSrv){
@@ -21,17 +21,17 @@ angular.module('app', ['ui.router'])
         })
         .state('character', {
             url: '/characters/:charId',
-            templateUrl: '/js/details/detail.html',
+            templateUrl: 'js/details/detail.html',
             controller: 'detailCtrl',
         })
         .state('map', {
             url: '/map',
-            templateUrl: '/js/map/map.html',
+            templateUrl: 'js/map/map.html',
             controller: 'mapCtrl'
         })
         .state('about', {
             url: '/about',
-            templateUrl: '/js/about/about.html',
+            templateUrl: 'js/about/about.html',
             controller: 'aboutCtrl',
         });
         
